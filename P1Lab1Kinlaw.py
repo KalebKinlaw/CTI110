97d5336e386d14bb1032c22c5baa4231567ceb95
KalebKinlaw <143745473+KalebKinlaw@users.noreply.github.com>
2023-12-14 13:47:58 -0500

    Add files via upload

---
+++ P1Lab1Kinlaw.py
@@ -0,0 +1,14 @@
+"""
+CTI 110
+P1LAB1
+Kinlaw
+8/24/2023
+"""
+
+# Say hello to the user
+print("Hello! What's your name?")
+
+#get the user's name 
+name = input ()
+
+print(name, "is a nice name")
---
+++ P1T2Kinlaw.py
@@ -0,0 +1,47 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+t = turtle.Turtle()
+#Comment block
+
+"""
+t.color("blue")
+t.pensize(3)
+#t.penup()
+t.forward(100)
+"""
+
+"""
+t.color("darkred")
+t.pensize(3)
+#t.penup()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+t.color ("blue")
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+"""
+
+t.color("darkred")
+t.circle(50)
+t.color("darkblue")
+t.circle(100)
+t.color("darkgreen")
+t.circle(150)
